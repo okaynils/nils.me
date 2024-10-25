7927d43fe180c557168fe8e32a9e6afed3e6b4a2
okaynils <nils.fahrni@students.fhnw.ch>
2024-10-25 17:53:15 +0200

    made timespan not selectable

--- components/experienceList.js
+++ components/experienceList.js
@@ -1,38 +1,56 @@
 export const ExperienceList = () => {
     return (
         <section class="space-y-4">
-            <div class="flex items-center space-x-4">
+            <div class="flex items-center space-x-4 group">
                 <img src="/images/pages/home/swiss_post.png" alt="Swiss Post" class="w-10 h-10 rounded-md" />
                 <div class="flex-grow">
-                    <a href="https://www.post.ch/en" target="_blank"><h2 class="font-semibold">Swiss Post</h2></a>
+                    <a href="https://www.post.ch/en" target="_blank">
+                        <h2 class="font-semibold">Swiss Post</h2>
+                    </a>
                     <p class="text-sm text-gray-500">Software Engineering Apprentice</p>
                 </div>
-                <span class="text-xs text-gray-400 ml-auto self-center">SUMMER 2017 — SUMMER 2021</span>
+                <span class="text-xs text-gray-400 ml-auto self-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 select-none">
+                    SUMMER 2017 — SUMMER 2021
+                </span>
             </div>
-            <div class="flex items-center space-x-4">
+            <div class="flex items-center space-x-4 group">
                 <img src="/images/pages/home/postfinance.png" alt="PostFinance" class="w-10 h-10 rounded-md" />
                 <div class="flex-grow">
-                    <a href="https://www.postfinance.ch/en" target="_blank"><h2 class="font-semibold">PostFinance</h2></a>
+                    <a href="https://www.postfinance.ch/en" target="_blank">
+                        <h2 class="font-semibold">PostFinance</h2>
+                    </a>
                     <p class="text-sm text-gray-500">Software Engineering Apprentice</p>
                 </div>
-                <span class="text-xs text-gray-400 ml-auto self-center">SUMMER 2018 — SPRING 2019</span>
+                <span class="text-xs text-gray-400 ml-auto self-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 select-none">
+                    SUMMER 2018 — SPRING 2019
+                </span>
             </div>
-            <div class="flex items-center space-x-4">
-                <img src="/images/pages/home/wks.jpeg" alt="PostFinance" class="w-10 h-10 rounded-md" />
+            <div class="flex items-center space-x-4 group">
+                <img src="/images/pages/home/wks.jpeg" alt="WKS Bern" class="w-10 h-10 rounded-md" />
                 <div class="flex-grow">
-                    <a href="https://www.wksbern.ch/" target="_blank"><h2 class="font-semibold">WKS Bern</h2></a>
+                    <a href="https://www.wksbern.ch/" target="_blank">
+                        <h2 class="font-semibold">WKS Bern</h2>
+                    </a>
                     <p class="text-sm text-gray-500">Matura Student</p>
                 </div>
-                <span class="text-xs text-gray-400 ml-auto self-center">SUMMER 2021 — SUMMER 2022</span>
+                <span class="text-xs text-gray-400 ml-auto self-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 select-none">
+                    SUMMER 2021 — SUMMER 2022
+                </span>
             </div>
-            <div class="flex items-center space-x-4">
-                <img src="/images/pages/home/fhnw.jpg" alt="PostFinance" class="w-10 h-10 rounded-md" />
+            <div class="flex items-center space-x-4 group">
+                <img src="/images/pages/home/fhnw.jpg" alt="FHNW" class="w-10 h-10 rounded-md" />
                 <div class="flex-grow">
-                    <a href="https://www.fhnw.ch/en/" target="_blank"><h2 class="font-semibold">FHNW</h2></a>
+                    <a href="https://www.fhnw.ch/en/" target="_blank">
+                        <h2 class="font-semibold">FHNW</h2>
+                    </a>
                     <p class="text-sm text-gray-500">BSc Data Science Student</p>
                 </div>
-                <span class="text-xs text-gray-400 ml-auto self-center">FALL 2022 —  SUMMER 2025</span>
+                <span class="text-xs text-gray-400 ml-auto self-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 select-none">
+                    FALL 2022 — SUMMER 2025
+                </span>
             </div>
         </section>
+
+
     );
 }
